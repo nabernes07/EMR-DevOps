e90a717595e799163868114c30c4a63096b71ecc
nabernes07 <99719692+nabernes07@users.noreply.github.com>
2023-05-17 13:26:15 +0530

    final

--- main.tf
+++ main.tf
@@ -59,6 +59,50 @@ resource "aws_emr_studio" "uws-emrserverless-studio-nonprod" {
   workspace_security_group_id = "sg-0cab79414ed325660"
 }
 
+resource "aws_emrserverless_application" "click_log_loggregator_emr_serverless" {
+  name          = "uws-testing-application"
+  release_label = "emr-6.9.0"
+  type          = "spark"
+
+  initial_capacity {
+    initial_capacity_type = "Driver"
+
+    initial_capacity_config {
+      worker_count = 1
+      worker_configuration {
+        cpu    = "4 vCPU"
+        memory = "20 GB"
+      }
+    }
+  }
+
+  initial_capacity {
+    initial_capacity_type = "Executor"
+
+    initial_capacity_config {
+      worker_count = 3
+      worker_configuration {
+        cpu    = "4 vCPU"
+        memory = "20 GB"
+      }
+    }
+  }
+
+  maximum_capacity {
+    cpu    = "2000 vCPU"
+    memory = "10000 GB"
+  }
+
+  network_configuration = {
+    subnet_ids = ["subnet-0acd8897043418623", "subnet-0e4ad91050601aa5a", "subnet-08997f2bcdad53c98"]
+  }
+
+  tags = {
+     application-name   = "uws"
+     environment-type   = "non-prod"
+  }
+}
+
 terraform {
   backend "s3" {
     bucket = "ecs-terraform-bernes"

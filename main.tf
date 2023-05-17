13a59b5c0d1d9a9ee0a3f3bd5605d7776c5d0048
nabernes07 <99719692+nabernes07@users.noreply.github.com>
2023-05-17 10:28:22 +0530

    commit1

--- main.tf
+++ main.tf
@@ -1,23 +1,47 @@
+resource "aws_iam_role" "emr_notebook_role" {
+  name = "emr-notebook-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "elasticmapreduce.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "emr_notebook_role_policy_attachment" {
+  role       = aws_iam_role.emr_notebook_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonElasticMapReduceEditorsRole"
+}
+
 resource "aws_emr_studio" "uws-emrserverless-studio-nonprod" {
   auth_mode                   = "IAM"
   default_s3_location         = "s3://cloudops-emrserverless/emr-serverless/"
-  engine_security_group_id    = sg-077a2d1f378103407
+  engine_security_group_id    = "sg-077a2d1f378103407"
   name                        = "uws-emrserverless-studio-nonprod"
-  service_role                = arn:aws:iam::904541710863:role/emr-notebook-role
-  subnet_ids                  = [subnet-0091d45f89f2e267b, subnet-01fe66d8d5b0efb18]
-  vpc_id                      = vpc-006438faf6005e56c
-  workspace_security_group_id = sg-0e63af6afb024313a
+  service_role                = aws_iam_role.emr_notebook_role.arn
+  subnet_ids                  = ["subnet-0091d45f89f2e267b", "subnet-01fe66d8d5b0efb18"]
+  vpc_id                      = "vpc-006438faf6005e56c"
+  workspace_security_group_id = "sg-0e63af6afb024313a"
 }
- 
 
 resource "aws_emrserverless_application" "click_log_loggregator_emr_serverless" {
   name          = "uws-testing-application"
   release_label = "emr-6.9.0"
   type          = "spark"
- 
+
   initial_capacity {
     initial_capacity_type = "Driver"
- 
+
     initial_capacity_config {
       worker_count = 1
       worker_configuration {
@@ -26,10 +50,10 @@ resource "aws_emrserverless_application" "click_log_loggregator_emr_serverless" 
       }
     }
   }
- 
+
   initial_capacity {
     initial_capacity_type = "Executor"
- 
+
     initial_capacity_config {
       worker_count = 3
       worker_configuration {
@@ -38,16 +62,18 @@ resource "aws_emrserverless_application" "click_log_loggregator_emr_serverless" 
       }
     }
   }
- 
+
   maximum_capacity {
     cpu    = "2000 vCPU"
     memory = "10000 GB"
   }
-network_configuration = {
+
+  network_configuration = {
     subnet_ids = ["subnet-04678cf437bfdfc99", "subnet-0702bed9041a4a61e", "subnet-0007ae6c2e9652b76"]
   }
+
   tags = {
-    nbcu:application-name = "uws"
-    nbcu:enviornment-type = "non-prod"
+    "nbcu:application-name"    = "uws"
+    "nbcu:environment-type"    = "non-prod"
   }
 }

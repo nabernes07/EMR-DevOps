e5f2b7d33bfc64e670e25a7cb1d1820579d51230
nabernes07 <99719692+nabernes07@users.noreply.github.com>
2023-05-17 11:40:44 +0530

    commit1

--- main.tf
+++ main.tf
@@ -2,84 +2,16 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_iam_role_policy" "emr_notebook_role_policy" {
-  name   = "emr-notebook-role-policy"
-  role   = aws_iam_role.emr_notebook_role.name
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": [
-        "s3:ListBucket",
-        "s3:GetObject"
-      ],
-      "Resource": [
-        "arn:aws:s3:::ecs-terraform-bernes",
-        "arn:aws:s3:::ecs-terraform-bernes/*"
-      ]
-    }
-  ]
-}
-EOF
-}
-
 resource "aws_emr_studio" "uws-emrserverless-studio-nonprod" {
   auth_mode                   = "IAM"
-  default_s3_location         = "s3://ecs-terraform-bernes/emr-serverless/"
-  engine_security_group_id    = "sg-0cab79414ed325660"
+  default_s3_location         = "s3://ecs-terraform-bernes/test"
+  engine_security_group_id    = "sg-049092e56541cf6d8"
   name                        = "uws-emrserverless-studio-nonprod"
   service_role                = "arn:aws:iam::068003677592:role/aws-service-role/ops.emr-serverless.amazonaws.com/AWSServiceRoleForAmazonEMRServerless"
-  subnet_ids                  = ["subnet-0acd8897043418623", "subnet-0e4ad91050601aa5a"]
+  subnet_ids                  = ["subnet-0acd8897043418623","subnet-0e4ad91050601aa5a"]
   vpc_id                      = "vpc-033ab8d7e34db0f84"
-  workspace_security_group_id = "sg-0e63af6afb024313a"
+  workspace_security_group_id = "sg-0cab79414ed325660"
 }
-
-resource "aws_emrserverless_application" "click_log_loggregator_emr_serverless" {
-  name          = "uws-testing-application"
-  release_label = "emr-6.9.0"
-  type          = "spark"
-
-  initial_capacity {
-    initial_capacity_type = "Driver"
-
-    initial_capacity_config {
-      worker_count = 1
-      worker_configuration {
-        cpu    = "4 vCPU"
-        memory = "20 GB"
-      }
-    }
-  }
-
-  initial_capacity {
-    initial_capacity_type = "Executor"
-
-    initial_capacity_config {
-      worker_count = 3
-      worker_configuration {
-        cpu    = "4 vCPU"
-        memory = "20 GB"
-      }
-    }
-  }
-
-  maximum_capacity {
-    cpu    = "2000 vCPU"
-    memory = "10000 GB"
-  }
-
-  network_configuration {
-    subnet_ids = ["subnet-0acd8897043418623", "subnet-0e4ad91050601aa5a", "subnet-08997f2bcdad53c98"]
-  }
-
-  tags = {
-    "nbcu:application-name"     = "uws"
-    "nbcu:environment-type"     ="non-prod"
-  }
-}
-
 
 
 terraform {

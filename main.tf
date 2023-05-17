79fe4919a9a8edcce695123e9d5078e03b0c3ad4
nabernes07 <99719692+nabernes07@users.noreply.github.com>
2023-05-17 11:56:12 +0530

    ji

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_iam_role" "emr_studio_role" {
-  name = "emr-studio-role"
+resource "aws_iam_role" "nonprod_role" {
+  name = "nonprod-role"
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -21,8 +21,8 @@ resource "aws_iam_role" "emr_studio_role" {
 EOF
 }
 
-resource "aws_iam_policy" "emr_studio_policy" {
-  name        = "emr-studio-policy"
+resource "aws_iam_policy" "nonprod_policy" {
+  name        = "nonprod-policy"
   description = "Policy for EMR Studio"
 
   policy = <<EOF
@@ -44,8 +44,8 @@ EOF
 }
 
 resource "aws_iam_role_policy_attachment" "emr_studio_role_policy_attachment" {
-  role       = aws_iam_role.emr_studio_role.name
-  policy_arn = aws_iam_policy.emr_studio_policy.arn
+  role       = aws_iam_role.nonprod_role.name
+  policy_arn = aws_iam_policy.nonprod_policy.arn
 }
 
 resource "aws_emr_studio" "uws-emrserverless-studio-nonprod" {
@@ -53,7 +53,7 @@ resource "aws_emr_studio" "uws-emrserverless-studio-nonprod" {
   default_s3_location         = "s3://ecs-terraform-bernes/test"
   engine_security_group_id    = "sg-049092e56541cf6d8"
   name                        = "uws-emrserverless-studio-nonprod"
-  service_role                = aws_iam_role.emr_studio_role.arn
+  service_role                = aws_iam_role.nonprod_role.arn
   subnet_ids                  = ["subnet-0acd8897043418623", "subnet-0e4ad91050601aa5a"]
   vpc_id                      = "vpc-033ab8d7e34db0f84"
   workspace_security_group_id = "sg-0cab79414ed325660"

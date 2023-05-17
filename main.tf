ecd7789cd71535dc34eebb8a8ef1363953181f8b
nabernes07 <99719692+nabernes07@users.noreply.github.com>
2023-05-17 11:47:05 +0530

    comit2

--- main.tf
+++ main.tf
@@ -1,5 +1,48 @@
-provider "aws" {
-  region = "ap-south-1"
+resource "aws_iam_role" "emr_studio_role" {
+  name = "emr-studio-role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "emr-containers.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "emr_studio_policy" {
+  name        = "emr-studio-policy"
+  description = "Policy for EMR Studio"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:ListBucket",
+        "s3:GetObject",
+        "s3:PutObject"
+      ],
+      "Resource": "arn:aws:s3:::ecs-terraform-bernes/*"
+    }
+    // Add more permissions as needed
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "emr_studio_role_policy_attachment" {
+  role       = aws_iam_role.emr_studio_role.name
+  policy_arn = aws_iam_policy.emr_studio_policy.arn
 }
 
 resource "aws_emr_studio" "uws-emrserverless-studio-nonprod" {
@@ -7,17 +50,16 @@ resource "aws_emr_studio" "uws-emrserverless-studio-nonprod" {
   default_s3_location         = "s3://ecs-terraform-bernes/test"
   engine_security_group_id    = "sg-049092e56541cf6d8"
   name                        = "uws-emrserverless-studio-nonprod"
-  service_role                = "arn:aws:iam::068003677592:role/aws-service-role/ops.emr-serverless.amazonaws.com/AWSServiceRoleForAmazonEMRServerless"
-  subnet_ids                  = ["subnet-0acd8897043418623","subnet-0e4ad91050601aa5a"]
+  service_role                = aws_iam_role.emr_studio_role.arn
+  subnet_ids                  = ["subnet-0acd8897043418623", "subnet-0e4ad91050601aa5a"]
   vpc_id                      = "vpc-033ab8d7e34db0f84"
   workspace_security_group_id = "sg-0cab79414ed325660"
 }
 
-
 terraform {
-    backend "s3" {
+  backend "s3" {
     bucket = "ecs-terraform-bernes"
-    key = "EMR-DevOps/terraform.tfstate"
+    key    = "EMR-DevOps/terraform.tfstate"
     region = "ap-south-1"
   }
 }
